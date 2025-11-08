82c7c5a6feadeb7ae676f2f3635bd02370d04618
Descapades <Destin.Champlin@gmail.com>
2025-11-08 08:51:51 -0500

    created welcome/ login screen, set up splash to welcome navigation

---
+++ app/src/main/java/com/example/pawtytime/WelcomeActivity.kt
@@ -0,0 +1,25 @@
+package com.example.pawtytime
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        findViewById<MaterialButton>(R.id.btnGoogle).setOnClickListener {
+                Snackbar.make(it, "Google sign-in coming next", Snackbar.LENGTH_SHORT).show()
+            }
+        findViewById<MaterialButton>(R.id.btnFacebook).setOnClickListener {
+                Snackbar.make(it, "Facebook sign-in coming next", Snackbar.LENGTH_SHORT).show()
+            }
+        findViewById<MaterialButton>(R.id.btnEmailLogin).setOnClickListener {
+                Snackbar.make(it, "Email login coming next", Snackbar.LENGTH_SHORT).show()
+            }
+
+    }
+}
