dd494a903c4cab0acfe26ea8ce75ad98e2425410
Kateland <KMoreira@student.fullsail.edu>
2025-11-11 21:44:53 -0600

    Got the toolbar set up, along with the buttons. Profile button is not set up yet

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val profileBtn = findViewById<ImageButton>(R.id.profile_btn)
+        val notifsBtn =findViewById<ImageButton>(R.id.notifs_btn)
+        val inboxBtn = findViewById<ImageButton>(R.id.inbox_btn)
+
         loadFragment(HomeScreen())
         bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)!!
         bottomNav.setOnItemSelectedListener {
@@ -37,6 +43,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
         bottomNav.itemIconTintList = null
+
+        profileBtn.setOnClickListener{
+
+        }
+
+        notifsBtn.setOnClickListener{
+            loadFragment(Notifications())
+        }
+
+        inboxBtn.setOnClickListener{
+            loadFragment(Inbox())
+        }
+
+
+
     }
     private  fun loadFragment(fragment: Fragment){
         val transaction = supportFragmentManager.beginTransaction()
