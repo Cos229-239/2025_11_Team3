36430ece0aca0bc51059d8a0f885b74bced775f8
Kateland <KMoreira@student.fullsail.edu>
2025-11-12 23:36:10 -0600

    Create and Implemented popup screen into mainActivity, made the buttons functional.2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
